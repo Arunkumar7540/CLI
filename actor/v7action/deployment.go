03ec482f5a88ec8e6e3c800c9ba0c2a227e65b64
arun kumar <lakshmana.arun@gmail.com>
2019-06-12 13:18:45 +0000

    Deleted actor

--- actor/v7action/deployment.go
+++
@@ -1,7 +0,0 @@
-package v7action
-
-func (actor Actor) CreateDeployment(appGUID string, dropletGUID string) (string, Warnings, error) {
-	deploymentGUID, warnings, err := actor.CloudControllerClient.CreateApplicationDeployment(appGUID, dropletGUID)
-
-	return deploymentGUID, Warnings(warnings), err
-}
